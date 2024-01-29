afd2955a12acc5d59f8a6aaaa9792e9f472f3b17
Ashu Negi <anegi6723@gmail.com>
2024-01-29 19:27:39 +0530

    adding middleware for static files

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"/views"));
 app.engine("ejs",ejsMate);
 app.use(express.urlencoded({extended : true}));
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.get("/", (req,res)=>{
     res.send("Root is wotking");
