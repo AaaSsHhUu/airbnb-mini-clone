5ccb9da2b673e7e93751e23f0b78f2736150a4d8
Ashu Negi <anegi6723@gmail.com>
2024-02-10 06:51:57 +0530

    adding post mongoose middleware to listings

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review");
 
 const listingSchema = new Schema({
     title : {
@@ -22,6 +23,13 @@ const listingSchema = new Schema({
             ref : "Review"
         }
     ]
+})
+
+// post middleware of mongoose to delete all review if listing is deleted
+listingSchema.post("findOneAndDelete", async (listing)=>{
+    if(listing){
+        await Review.deleteMany({_id : {$in : listing.reviews}})
+    }
 })
 
 const Listing = mongoose.model("Listing",listingSchema);
