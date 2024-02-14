f86c8bbc8e95a4a62cbe5604fd2b54ce89734911
Ashu Negi <anegi6723@gmail.com>
2024-02-14 19:30:25 +0530

    created signup page

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const { route } = require("./listing");
-const router = express.Router({mergeParams : true})
+const router = express.Router({mergeParams : true});
+const User = require("../models/user");
+const wrapAsync = require("../utils/wrapAsync");
 
 router.get("/signup",(req,res)=>{
-    res.send("form");
+    res.render("users/signup.ejs");
 })
+
+router.post("/signup", wrapAsync(async (req,res)=>{
+    try{
+        let {username,email,password} = req.body;
+        const newUser = new User({username,email});
+        const registeredUser = await User.register(newUser,password);
+        console.log(registeredUser);
+        req.flash("success","user registered successfully");
+        res.redirect("/listings");
+    }
+    catch(err){
+        req.flash("error",err.message);
+        res.redirect("/signup");
+    }
+}))
 
 module.exports = router;
