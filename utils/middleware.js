513f11ce9b3b1af09a675e08748a99bc4aff102d
Ashu Negi <anegi6723@gmail.com>
2024-02-18 05:17:17 +0530

    middleware for savign redirect url

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,7 +1,16 @@
 module.exports.isLoggedIn = (req,res,next) => {
     if(!req.isAuthenticated()){
+        req.session.redirectUrl = req.originalUrl;
         req.flash("error","You must be logged in to create a listing");
         res.redirect("/login");
+    }
+    next();
+}
+
+// as on successful login, passport resets the session therefore redirectUrl will come undefined, so we have to save it in locals.
+module.exports.saveRedirectUrl = (req,res,next)=>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl;
     }
     next();
 }
