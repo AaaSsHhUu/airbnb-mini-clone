e45ca083d0664b5f283c92f40bf1a64fe408cb60
Ashu Negi <anegi6723@gmail.com>
2024-02-18 06:21:28 +0530

    showing owner info in show.ejs

--- routes/listing.js
+++ routes/listing.js
@@ -34,6 +34,7 @@ router.post("/",isLoggedIn ,validateListing, wrapAsync(
     async (req, res, next) => {
         // let {title,description,image,price,location,country} = req.body; 
         const newListing = new Listing(req.body.listing);
+        newListing.owner = req.user._id;
         await newListing.save();
         req.flash("success","New Listing Created!!!");
         res.redirect("/listings");
@@ -45,7 +46,7 @@ router.post("/",isLoggedIn ,validateListing, wrapAsync(
 // Show Route
 router.get("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
+    const listing = await Listing.findById(id).populate("reviews").populate("owner");
     if(!listing){
         req.flash("error","Your requested listing does not exist");
         res.redirect('/listings');
