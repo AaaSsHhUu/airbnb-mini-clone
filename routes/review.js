c9182294167a3b875af8398bf461227aaa08dcad
Ashu Negi <anegi6723@gmail.com>
2024-02-19 17:58:23 +0530

    use middlewares for authorization

--- routes/review.js
+++ routes/review.js
@@ -3,15 +3,15 @@ const router = express.Router({mergeParams : true}); // mergerParams preserves t
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js")
 const wrapAsync = require("../utils/wrapAsync.js");
-const {validateReview} = require("../utils/middleware.js");
+const {validateReview, isLoggedIn, isReviewAuthor} = require("../utils/middleware.js");
 
 
 
 // Review route
-router.post("/", validateReview, wrapAsync(async (req, res) => {
+router.post("/",isLoggedIn, validateReview, wrapAsync(async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
-
+    newReview.author = req.user._id;
     listing.reviews.push(newReview);
 
     await newReview.save();
@@ -22,7 +22,7 @@ router.post("/", validateReview, wrapAsync(async (req, res) => {
 }))
 
 // Delete review
-router.delete("/:reviewId", wrapAsync(async (req, res) => {
+router.delete("/:reviewId",isReviewAuthor, wrapAsync(async (req, res) => {
     let { id, reviewId } = req.params;
     await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
     await Review.findByIdAndDelete(reviewId);
