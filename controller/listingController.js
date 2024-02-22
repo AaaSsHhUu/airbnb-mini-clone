0e806815fbce43725aacec26a46d6d22fcb73958
Ashu Negi <anegi6723@gmail.com>
2024-02-22 19:23:43 +0530

    modifying newData in listing controller

--- controller/listingController.js
+++ controller/listingController.js
@@ -11,8 +11,11 @@ const newListing = (req, res) => {
 
 const newData = async (req, res, next) => {
     // let {title,description,image,price,location,country} = req.body; 
+    let url = req.file.path;
+    let filename = req.file.filename;
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
+    newListing.image = {url, filename};
     await newListing.save();
     req.flash("success","New Listing Created!!!");
     res.redirect("/listings");
